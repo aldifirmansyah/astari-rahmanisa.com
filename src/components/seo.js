d68ef28f78cc764b7ef6587ec2bf5875833da8cc
Aldi Firmansyah <aldifirmansyah@ruangguru.com>
2020-05-30 12:07:20 +0700

    init project

---
+++ src/components/seo.js
@@ -0,0 +1,18 @@
+import React from 'react'
+import {Helmet} from 'react-helmet'
+
+const SEO = props => {
+  const name = 'Astari Dwi Rahmanisa';
+  const description = 'Hello! I am Astari and this is my UI/UX Portfolio.';
+
+  const title = props.pageName ? `${props.pageName} | ${name}` : name;
+
+  return (
+    <Helmet>
+      <title>{title}</title>
+      <meta name='description' content={props.description ? props.description : description}/>
+    </Helmet>
+  )
+}
+
+export default SEO;
