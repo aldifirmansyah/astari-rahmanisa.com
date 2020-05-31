391ead24e74a12800704c899ec91af346ea3ef15
Aldi Firmansyah <aldifirmansyah@ruangguru.com>
2020-05-31 12:37:56 +0700

    Add SubContainer and profile picture section in about page

---
+++ src/components/ProfilePicture/index.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import styled from 'styled-components';
+import ImageWithSkeleon from '../ImageWithSkeleton';
+import Picture from '../../assets/images/profile/image.jpg';
+import Skeleton from '../../assets/images/profile/image_skeleton.jpg';
+
+const PictureContainer = styled.section`
+  width: 100%;
+  margin: 160px 0 0 0;
+`;
+
+const ProfilePicture = () => (
+  <PictureContainer>
+    <ImageWithSkeleon width='100%' src={Picture} skeletin={Skeleton}/>
+  </PictureContainer>
+)
+
+export default ProfilePicture;
--- src/containers/About/index.js
+++ src/containers/About/index.js
@@ -1,9 +1,21 @@
 import React from 'react';
+import styled from 'styled-components';
 import SEO from '../../components/seo';
+import ProfilePicture from '../../components/ProfilePicture';
+
+const SubContainer = styled.div`
+  margin: 0 120px;
+  @media (max-width: 768px) {
+    margin: 0;
+  }
+`;
 
 const About = () => {
   return (
+    <SubContainer>
     <SEO title='About'/>
+    <ProfilePicture/>
+    </SubContainer>
   )
 }
 
