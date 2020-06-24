3feab84906fd6aed7c9b6f742a9bb8e854e37dcc
Aldi Firmansyah <aldifirmansyah@ruangguru.com>
2020-06-24 20:07:57 +0700

    Add bogorsehat thumbnail

--- src/components/Thumbnails/data.js
+++ src/components/Thumbnails/data.js
@@ -1,5 +1,7 @@
 import tjetakKnowledgePhoto from '../../assets/images/thumbnails/tjetak_knowledge.jpg';
 import tjetakKnowledgeSkeleton from '../../assets/images/thumbnails/tjetak_knowledge_skeleton.jpg';
+import bogorSehatPhoto from '../../assets/images/thumbnails/bogorsehat.jpg';
+import bogorSehatSkeleton from '../../assets/images/thumbnails/bogorsehat_skeleton.jpg';
 
 const data = [
   {
@@ -8,6 +10,13 @@ const data = [
     title: 'Tjetak Knowledge',
     description: 'Tjetak Knowledge is a Knowledge Management System designed for the entire company of Tjetak',
     createdDate: 'May 3rd, 2020'
+  },
+  {
+    photo: bogorSehatPhoto,
+    skeleton: bogorSehatSkeleton,
+    title: 'Bogor Sehat',
+    description: 'To help Indonesia enhance their health quality, my team and I designed a mobile health application for citizen of Bogor.',
+    createdDate: 'November 22nd, 2019'
   }
 ]
 
