6798de5d5954012a62c668fff413b8275cfca4fb
Aldi Firmansyah <aldifirmansyah@ruangguru.com>
2020-06-24 20:06:07 +0700

    Remove tjetak experience from thumbnail in work page

--- src/components/Thumbnails/data.js
+++ src/components/Thumbnails/data.js
@@ -1,7 +1,5 @@
 import tjetakKnowledgePhoto from '../../assets/images/thumbnails/tjetak_knowledge.jpg';
 import tjetakKnowledgeSkeleton from '../../assets/images/thumbnails/tjetak_knowledge_skeleton.jpg';
-import tjetakExperiencePhoto from '../../assets/images/thumbnails/tjetak_experience.jpg';
-import tjetakExperienceSkeleton from '../../assets/images/thumbnails/tjetak_experience_skeleton.jpg';
 
 const data = [
   {
@@ -10,13 +8,6 @@ const data = [
     title: 'Tjetak Knowledge',
     description: 'Tjetak Knowledge is a Knowledge Management System designed for the entire company of Tjetak',
     createdDate: 'May 3rd, 2020'
-  },
-  {
-    photo: tjetakExperiencePhoto,
-    skeleton: tjetakExperienceSkeleton,
-    title: 'Work Experience at Tjetak',
-    description: 'In my past summer 2019, I worked as a UI Designer Intern at Tjetak, a tech-based packaging platform.',
-    createdDate: 'March 31st, 2020'
   }
 ]
 
